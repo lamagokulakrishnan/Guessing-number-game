bf64ab326f591c594297b00319bb08a1d2f93d10
LamaGokulakrishnan <83019892+lamagokulakrishnan@users.noreply.github.com>
2024-12-08 13:05:44 +0530

    Update GameResultRepository.java
    Repository added best score

--- src/main/java/com/lama/guesgame/repository/GameResultRepository.java
+++ src/main/java/com/lama/guesgame/repository/GameResultRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface GameResultRepository extends JpaRepository<GameResult, Long> {
     Optional<GameResult> findTopByOrderByScoreAsc(); // Fetch the best player based on the score
+
+Optional<Game> findTopByOrderByScoreAsc();  // Assuming score is a combined metric of time and moves
 }
